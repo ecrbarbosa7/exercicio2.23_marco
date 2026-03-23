68df44a20cd2fe49c24590fe27afc6cdebac74a2
neuza <neuzap963@gmail.com>
2026-03-23 13:38:00 +0100

    Alumno B: mensage descifrado

---
+++ assents/js/alumno_b.js
@@ -0,0 +1,7 @@
+// TRABAJO DEL ALUMNO B
+
+// Montar a frase na ordem correta
+const mensajeOrdenado = piezas[1] + " " + piezas[2] + " " + piezas[0];
+
+console.log("--- MENSAJE DECODIFICADO ---");
+console.log(mensajeOrdenado);
